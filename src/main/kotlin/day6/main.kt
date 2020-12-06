cd923576325b7c7be0bc86e26d32d7529c1e3b29
loosbrecht <loos.brecht@gmail.com>
2020-12-06 08:33:39 +0100

    Day 6: Solved
    went a bit overboard with transforming everything to a stream. But it sure does look nice

---
+++ src/main/kotlin/day6/main.kt
@@ -0,0 +1,42 @@
+package main.kotlin.day6
+
+import java.io.File
+
+fun main() {
+    val lines = readFileToLines("input/day6.txt")
+    val totalAnswers = collectAllAnswers(lines).map { countAnswersFromGroup(it) }.sum()
+    val totalAnswersInCommon = collectAllAnswers(lines).map { countAnswersInCommon(it) }.sum()
+    println(totalAnswers)
+    println(totalAnswersInCommon)
+}
+
+fun collectAllAnswers(lines: List<String>): List<List<String>> {
+    val groups = mutableListOf<List<String>>()
+    var persons = mutableListOf<String>()
+    for (line in lines) {
+        if (line.isEmpty()) {
+            groups.add(persons)
+            persons = mutableListOf()
+            continue
+        }
+        persons.add(line)
+    }
+    groups.add(persons)
+    return groups
+}
+
+fun countAnswersInCommon(groups: List<String>): Int {
+    val memberCount = groups.size
+    return ('a'..'z').count { ch -> groups.filter { it.contains(ch) }.count() == memberCount }
+}
+
+fun countAnswersFromGroup(group: List<String>): Int {
+    val answers = mutableMapOf<Char, Boolean>()
+    group.forEach { p -> p.forEach { answers[it] = true } }
+    return answers.count()
+}
+
+
+fun readFileToLines(fileName: String): List<String> {
+    return File(fileName).readLines()
+}
